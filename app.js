042550768700a1aabb99d11720877f14dfcb536e
Juholuos <juholuostarihw@gmail.com>
2024-01-17 17:22:02 +0200

    eteenpäin

---
+++ app.js
@@ -0,0 +1,60 @@
+// const log = require('./logger');
+
+/*
+const path = require('path');
+var pathObj = path.parse(__filename);
+console.log(pathObj);
+*/
+
+/*
+const os = require(`os`);
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log(`Total memory: ${totalMemory}`);
+console.log(`Free memory: ${freeMemory}`);
+*/
+
+// const fs = require('fs');
+
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+/*
+fs.readdir('$', function(err, files) {
+  if(err) console.log('Error', err);
+  else console.log('Result', files)
+})
+*/
+
+/*
+const EventEmitter = require('events');
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+//  Register a listener
+logger.on('messageLogged', (arg) => {
+  console.log('Listener called', arg);
+});
+
+logger.log('message');
+*/
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/' ) {
+    res.write('Hello world');
+    res.end();
+  }
+
+  if(req.url === '/api/courses') {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
