2abe78aced2c38bf5ed5250c4fa1dcf2987419dc
Juholuos <juholuostarihw@gmail.com>
2024-02-02 22:37:56 +0200

    Traversy eteenpäin

--- traversy-media-express-course/index.js
+++ traversy-media-express-course/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const path = require('path');
 const logger = require('./middleware/logger')
 const app = express();
@@ -6,10 +7,15 @@ const app = express();
 // Init middleware
 //app.use(logger);
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded( { extended: false }));
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/members', require('./routes/api/api/members'));
+// Members API routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
