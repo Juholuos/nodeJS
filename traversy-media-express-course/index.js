cf49578df8c1280391266a071591c476669801e9
Juholuos <juholuostarihw@gmail.com>
2024-02-03 11:45:55 +0200

    Ongelmia form jutussa

--- traversy-media-express-course/index.js
+++ traversy-media-express-course/index.js
@@ -1,21 +1,32 @@
-const express = require('express');
-
-const path = require('path');
-const logger = require('./middleware/logger')
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const logger = require("./middleware/logger");
 const app = express();
+const members = require("./Members");
 
 // Init middleware
 //app.use(logger);
 
+// Handlebars middleware
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Body Parser Middleware
 app.use(express.json());
-app.use(express.urlencoded( { extended: false }));
+app.use(express.urlencoded({ extended: false }));
+
+// Homepage Route
+app.get("/", (req, res) => res.render("index", { 
+  title: "Member App",
+  members: members
+}));
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Members API routes
-app.use('/api/members', require('./routes/api/members'));
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
