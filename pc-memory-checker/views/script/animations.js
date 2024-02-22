c1bc0e3490eb3ea716184391e37e3955bce7156a
Juholuos <112057669+Juholuos@users.noreply.github.com>
2024-02-22 12:50:57 +0200

    aha

--- pc-memory-checker/views/script/animations.js
+++ pc-memory-checker/views/script/animations.js
@@ -1,30 +1,30 @@
-// Animate "loading..." text
-function animateLoadingDots() {
-  console.log('test');
-  const loadingDivs = document.querySelectorAll('.loading');
-  loadingDivs.forEach(div => {
-    let dots = ''
-    setInterval(() => {
-      div.textContent = 'Loading' + dots;
-      dots += '.';
-      if (dots.length > 3) {
-        dots = '';
-      }
-    }, 200);
-  })
-}
+// console.log('halo');
+// // Animate "loading..." text
+// function animateLoadingDots() {
+//   console.log('test');
+//   const loadingDivs = document.querySelectorAll('.loading');
+//   loadingDivs.forEach(div => {
+//     let dots = ''
+//     setInterval(() => {
+//       div.textContent = 'Loading' + dots;
+//       dots += '.';
+//       if (dots.length > 3) {
+//         dots = '';
+//       }
+//     }, 200);
+//   })
+// }
 
-console.log('halo');
 
-// Animate loading effect to bars
-function animateBars() {
-  console.log('test');
-  const loadingBars = document.querySelectorAll('.bar');
-  loadingBars.forEach(bar => {
-    bar.style.backgroundColor = "black";
-  });
-}
+// // Animate loading effect to bars
+// function animateBars() {
+//   console.log('test');
+//   const loadingBars = document.querySelectorAll('.bar');
+//   loadingBars.forEach(bar => {
+//     bar.style.backgroundColor = "black";
+//   });
+// }
 
-animateBars();
-animateLoadingDots();
+// animateBars();
+// animateLoadingDots();
 
