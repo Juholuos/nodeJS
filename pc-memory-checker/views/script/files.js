8a50bfced16302239ebf3156b06a8882b13106aa
Juholuos <juholuostarihw@gmail.com>
2024-02-26 16:35:11 +0200

    File manager tehty

---
+++ pc-memory-checker/views/script/files.js
@@ -0,0 +1,6 @@
+const fileBtn = document.querySelector('.file-btn');
+const fileInput = document.querySelector('.file-input')
+
+fileBtn.addEventListener('click', function() {
+  fileInput.click();
+});
