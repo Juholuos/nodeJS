31998ec4f5fe364739b8aa265591a9b36f380bdc
Juholuos <juholuostarihw@gmail.com>
2024-02-29 13:58:17 +0200

    Aloitettu

---
+++ weather-app/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+
+const express = require('express');
+const path = require('path');
+const { error } = require('console');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  console.log('halo');
+  res.send('hello world!')
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
+
