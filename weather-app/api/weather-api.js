a44c4c211f885d953d08131b6fcd22c31bdb92a5
Juholuos <juholuostarihw@gmail.com>
2024-03-02 13:56:59 +0200

    Toimii

--- weather-app/api/weather-api.js
+++ weather-app/api/weather-api.js
@@ -1,17 +1,30 @@
 const request = require('request');
 const argv = require('yargs').argv;
 
+const dayjs = require('dayjs');
+dayjs().format()
+
 let apiKey = '90c8e783d8cfa201bfc850f6d773f546'
 let city = argv.c || 'savonlinna';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
-request(url, function(err, response, body) {
-  if(err) {
-    console.log('error:', error);
-  } else {
-    let weather = JSON.parse(body)
+// request(url, function(err, response, body) {
+//   if(err) {
+//     console.log('error:', error);
+//   } else {
+//     let weather = JSON.parse(body)
 
-    let message = `${weather.main.temp} celsiusastetta paikkakunnalla ${weather.name}!`;
-    console.log(message);
-  }
-});
+//     let message = `${weather.main.temp} celsiusastetta paikkakunnalla ${weather.name}!`;
+//     console.log(message);
+//   }
+// });
+
+
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const location = data.name;
+    
+    
+  })
---
+++ weather-app/public/script/date.js
@@ -0,0 +1,10 @@
+// Date
+const currentDate = dayjs();
+
+const formattedDate = currentDate.format('HH:mm, DD.MM.YYYY')
+const nextHour = 
+
+
+module.exports = {
+  formattedDate: formattedDate
+}
